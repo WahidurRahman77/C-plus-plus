c8fef68ee051734368c05cb59b5dbcef868d9181
Wahidur Rahman <wrrahman456@gmail.com>
2024-10-15 14:20:42 +0600

    Add files via upload

---
+++ Two Screen.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int q;
+    cin >> q;
+    
+    while (q--) 
+    {
+        string s, t;
+        cin >> s >> t;
+        
+        int lcp = 0;
+        while (lcp < s.length() && lcp < t.length() && s[lcp] == t[lcp]) 
+        {
+            lcp++;
+        }
+        
+        int option1 = s.length() + (t.length() - lcp) + 1;
+        int option2 = s.length() + t.length();
+        
+        int result = min(option1, option2);
+        cout << result << endl;
+    }
+
+    return 0;
+}
