c5240475ca24fc8d0ce77526f9fea407d01e263d
Wahidur Rahman <wrrahman456@gmail.com>
2025-02-03 13:08:57 +0600

    Add files via upload

---
+++ Dstance Physics.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int v, t;
+    while(cin >> v >> t)
+    {
+        int s = 2*v*t;
+        cout << s << endl;
+    }
+
+    return 0;
+}
