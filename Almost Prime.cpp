b78ae818c52c800854973944ca5768dbcd59f8cd
Wahidur Rahman <wrrahman456@gmail.com>
2025-02-11 19:02:32 +0600

    Add files via upload

---
+++ Almost Prime.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num, c=0;
+    cin >> num;
+    for(int i = 2; i <= num; i++)
+    {
+        int primec=0;
+        int i1 = i;
+        for(int j = 2; j <= sqrt(i); j++)
+        {
+            if(i%j == 0)
+            {
+                primec++;
+                while(i%j == 0)
+                {
+                    i/=j;
+                }
+            }
+        }
+        if(i > 1) primec++;
+        if(primec ==2)
+        {
+            c++;
+        }
+        i = i1;
+    }
+    cout << c << endl;
+
+    return 0;
+}
