b6417b464fc896c9a6236e43e8b91b4753bae69d
Wahidur Rahman <wrrahman456@gmail.com>
2025-07-24 17:06:14 +0600

    Add files via upload

---
+++ A_Only_One_Digit.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios :: sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test;
+    cin >> test;
+    while (test--) 
+    {
+        int x;
+        cin >> x;
+        if(x <= 9)
+        {
+            cout << x << "\n";
+        }
+        else
+        {
+            int c = 9;
+            while(x > 0)
+            {
+                int a = x % 10;
+                x = x/10;
+                c = min(c, a);
+            }
+            cout << c << "\n";
+        }
+
+    }
+
+    return 0;
+}
